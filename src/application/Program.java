baac1897714d0e27ffa6a8d74179d408134e7513
Ronan Soares <ronanp2@hotmail.com>
2023-11-20 18:54:43 -0300

    Inserir implementação

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,44 +11,52 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		
-		SellerDao sellerDao = DaoFactory.createSellerDao(); //O programa não conhece a implementação só a interface.
-		
+
+		SellerDao sellerDao = DaoFactory.createSellerDao(); // O programa não conhece a implementação só a interface.
+
 		System.out.println("=== TESTE 1: Vendedor Procura por Id ===");
-		
+
 		Seller seller = sellerDao.findById(3);
-		
-		//System.out.println("DEPARTAMENTO");
-		//System.out.println("------------");
-		//System.out.println(dep.toString());
-		
+
+		// System.out.println("DEPARTAMENTO");
+		// System.out.println("------------");
+		// System.out.println(dep.toString());
+
 		System.out.println("======================================================");
-		
+
 		System.out.println("VENDEDOR");
 		System.out.println("--------");
 		System.out.println(seller.toString());
-		
+
 		System.out.println("======================================================");
-		
+
 		System.out.println("=== TESTE 2: Procura vendedor por Departamento ===");
 		System.out.println("------------------------------------------------------");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
-		
-		for(Seller obj : list) {
+
+		for (Seller obj : list) {
 			System.out.println(obj);
 			System.out.println("======================================================");
 		}
-		
-System.out.println("======================================================");
-		
+
+		System.out.println("======================================================");
+
 		System.out.println("=== TESTE 3: Procura todos vendedores ===");
 		System.out.println("------------------------------------------------------");
 		List<Seller> list2 = sellerDao.findAll();
-		
-		for(Seller obj : list2) {
+
+		for (Seller obj : list2) {
 			System.out.println(obj);
 			System.out.println("======================================================");
 		}
+
+		System.out.println("\n=== TEST 4: seller insert =====");
+		Seller newSeller = new Seller(null, "Nicolly Honorato David", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserido! Novo Id = " + newSeller.getId());
+		
+			System.out.println("======================================================");
+		
 	}
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -15,30 +16,64 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
-public class SellerDaoJDBC implements SellerDao{
-	
+public class SellerDaoJDBC implements SellerDao {
+
 	private Connection conn;
-	
+
 	public SellerDaoJDBC(Connection conn) {
 		this.conn = conn;
 	}
-	
+
 	@Override
 	public void insert(Seller dep) {
-		// TODO Auto-generated method stub
-		
+		PreparedStatement ps = null;
+		try {
+			ps = conn.prepareStatement(
+
+					"INSERT INTO seller " + "(Name, Email, BirthDate, BaseSalary, DepartmentId) " + "VALUES "
+							+ "(?, ?, ?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS); // Para retornar o Id.
+
+			ps.setString(1, dep.getName());
+			ps.setString(2, dep.getEmail());
+			ps.setDate(3, new java.sql.Date(dep.getBirthDate().getTime()));
+			ps.setDouble(4, dep.getBaseSalary());
+			ps.setInt(5, dep.getDepartment().getId());
+
+			int linhasAfetadas = ps.executeUpdate(); // variável recebe o nr de linhas e executa comd.
+
+			if (linhasAfetadas > 0) { // Se linha alteradas for maior que 0.
+				ResultSet rs = ps.getGeneratedKeys();
+				if (rs.next()) {
+					int id = rs.getInt(1); // Pega o Id da coluna 1
+					dep.setId(id);
+				}
+				
+				DB.closeResultSet(rs);
+			}
+			else {
+				throw new DbException("Erro inesperado! Nenhuma linha foi alterada");
+			}
+
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(ps);
+		}
+
 	}
 
 	@Override
 	public void update(Seller dep) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deleteById(Integer id) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -47,36 +82,31 @@ public class SellerDaoJDBC implements SellerDao{
 		ResultSet rs = null;
 		try {
 			ps = conn.prepareStatement(
-					"SELECT seller.*,department.Name as DepName "
-					+ "FROM seller INNER JOIN department "
-					+ "ON seller.DepartmentId = department.Id "
-					+ "WHERE seller.Id = ?");
-			
+					"SELECT seller.*,department.Name as DepName " + "FROM seller INNER JOIN department "
+							+ "ON seller.DepartmentId = department.Id " + "WHERE seller.Id = ?");
+
 			ps.setInt(1, id);
 			rs = ps.executeQuery();
-			
-			if(rs.next()) {
-				Department dep = instantiateDepartment(rs);				
+
+			if (rs.next()) {
+				Department dep = instantiateDepartment(rs);
 				Seller obj = instantiateSeller(rs, dep);
 				return obj;
-				
-				
+
 			}
 			return null;
-					
-		}
-		catch(SQLException e) {
+
+		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
-		}
-		finally {
+		} finally {
 			DB.closeStatement(ps);
 			DB.closeResultSet(rs);
-		}		
+		}
 	}
 
 	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
 		Seller obj = new Seller();
-		obj.setId(rs.getInt("Id"));	
+		obj.setId(rs.getInt("Id"));
 		obj.setName(rs.getString("Name"));
 		obj.setEmail(rs.getString("Email"));
 		obj.setBaseSalary(rs.getDouble("BaseSalary"));
@@ -98,38 +128,34 @@ public class SellerDaoJDBC implements SellerDao{
 		ResultSet rs = null;
 		try {
 			ps = conn.prepareStatement(
-					"SELECT seller.*,department.Name as DepName "
-					+ "FROM seller INNER JOIN department "
-					+ "ON seller.DepartmentId = department.Id "
-					+ "ORDER BY Name");
-			
+					"SELECT seller.*,department.Name as DepName " + "FROM seller INNER JOIN department "
+							+ "ON seller.DepartmentId = department.Id " + "ORDER BY Name");
+
 			rs = ps.executeQuery();
-			
+
 			// Como são varios valores, tem que declarar uma lista.
 			List<Seller> list = new ArrayList<>();
-			
+
 			// Estrutura Map para não repetir os departamentos.
 			Map<Integer, Department> map = new HashMap<>(); // Cria o map vazio
-			
-			while(rs.next()) {
-				
+
+			while (rs.next()) {
+
 				// guarda no map todo departamento que instanciar.
-				Department dep = map.get(rs.getInt("DepartmentId")); 
-				
-				if(dep == null) {
-					dep = instantiateDepartment(rs); //Se for nulo instancia o departamento.
+				Department dep = map.get(rs.getInt("DepartmentId"));
+
+				if (dep == null) {
+					dep = instantiateDepartment(rs); // Se for nulo instancia o departamento.
 					map.put(rs.getInt("DepartmentId"), dep); // Salva o departamento no dep.
 				}
-			
+
 				Seller obj = instantiateSeller(rs, dep);
-				list.add(obj);					
+				list.add(obj);
 			}
-			return list;					
-		}
-		catch(SQLException e) {
+			return list;
+		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
-		}
-		finally {
+		} finally {
 			DB.closeStatement(ps);
 			DB.closeResultSet(rs);
 		}
@@ -141,40 +167,35 @@ public class SellerDaoJDBC implements SellerDao{
 		ResultSet rs = null;
 		try {
 			ps = conn.prepareStatement(
-					"SELECT seller.*,department.Name as DepName "
-					+ "FROM seller INNER JOIN department "
-					+ "ON seller.DepartmentId = department.Id "
-					+ "WHERE DepartmentId = ? "
-					+ "ORDER BY Name");
-			
+					"SELECT seller.*,department.Name as DepName " + "FROM seller INNER JOIN department "
+							+ "ON seller.DepartmentId = department.Id " + "WHERE DepartmentId = ? " + "ORDER BY Name");
+
 			ps.setInt(1, department.getId());
 			rs = ps.executeQuery();
-			
+
 			// Como são varios valores, tem que declarar uma lista.
 			List<Seller> list = new ArrayList<>();
-			
+
 			// Estrutura Map para não repetir os departamentos.
 			Map<Integer, Department> map = new HashMap<>(); // Cria o map vazio
-			
-			while(rs.next()) {
-				
+
+			while (rs.next()) {
+
 				// guarda no map todo departamento que instanciar.
-				Department dep = map.get(rs.getInt("DepartmentId")); 
-				
-				if(dep == null) {
-					dep = instantiateDepartment(rs); //Se for nulo instancia o departamento.
+				Department dep = map.get(rs.getInt("DepartmentId"));
+
+				if (dep == null) {
+					dep = instantiateDepartment(rs); // Se for nulo instancia o departamento.
 					map.put(rs.getInt("DepartmentId"), dep); // Salva o departamento no dep.
 				}
-			
+
 				Seller obj = instantiateSeller(rs, dep);
-				list.add(obj);					
+				list.add(obj);
 			}
-			return list;					
-		}
-		catch(SQLException e) {
+			return list;
+		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
-		}
-		finally {
+		} finally {
 			DB.closeStatement(ps);
 			DB.closeResultSet(rs);
 		}
