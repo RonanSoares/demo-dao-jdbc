162a09d560057c7a377d851a902b59264f41cea8
Ronan Soares <ronanp2@hotmail.com>
2023-11-21 09:54:11 -0300

    Implementa apagar vendedor

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +12,8 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
 
 		SellerDao sellerDao = DaoFactory.createSellerDao(); // O programa não conhece a implementação só a interface.
 
@@ -66,5 +69,12 @@ public class Program {
 
 		System.out.println("======================================================");
 
+		System.out.println("\n=== TESTE 6: Excluir vendedor =====");
+		System.out.print("Informe o Id do vendedor: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Registro apagado com sucesso!");
+
+		sc.close();
 	}
 }
